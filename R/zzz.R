69d3b47b7f52ff5815c7c46033d284a15e9ad065
jonathanspeh <jspeh@gmx.de>
2024-08-28 11:07:09 +0200

    Modified python version and switched to conda-forge

--- R/zzz.R
+++ R/zzz.R
@@ -2,14 +2,14 @@
 .onAttach <- function(libname, pkgname){
               if(!reticulate::condaenv_exists("cellannealr-internal")){
                 packageStartupMessage("Creating cellannealr-internal conda environment and installing cellanneal and its dependencies")
-                reticulate::conda_create("cellannealr-internal", "python==3.10", channel = "anaconda")
+                reticulate::conda_create("cellannealr-internal", "python==3.08.19", channel = "conda-forge")
                 reticulate::conda_install("cellannealr-internal", c("numpy==1.24", "scipy==1.9",
                                                              "matplotlib==3.7", "pandas==1.5",
                                                              "seaborn==0.12"))
                 reticulate::conda_install("cellannealr-internal",
                                            c("openpyxl==3.0",
                                              "xlrd==1.2"),
-                                           channel = "anaconda")
+                                           channel = "conda-forge")
                 reticulate::use_condaenv("cellannealr-internal")
                 curl::curl_download("https://github.com/LiBuchauer/cellanneal/archive/refs/heads/master.zip", destfile = "./cellanneal.zip")
                 utils::unzip("cellanneal.zip")
